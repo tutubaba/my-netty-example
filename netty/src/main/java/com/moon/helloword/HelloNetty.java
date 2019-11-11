f631cf37d547f7d9c01f3996c26f9a4f42ab2529
tuyuelai <tuyuelai@szhtxx.com>
2019-11-11 19:34:10 +0800

    一个简单的netty代码

---
+++ netty/src/main/java/com/moon/helloword/HelloNetty.java
@@ -0,0 +1,29 @@
+package com.moon.helloword;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class HelloNetty {
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(10);
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+            .childHandler(new ServerInitializer());
+            ChannelFuture  f = serverBootstrap.bind(8888);
+            f.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
