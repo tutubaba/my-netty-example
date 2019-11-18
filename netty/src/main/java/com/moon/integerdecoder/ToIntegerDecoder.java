183f4740446b83782c05b2eb704cbe445d8b1718
tuyl <tuyuelai@szhtxx.com>
2019-11-18 21:46:13 +0800

    一个自定义编解码小例子

---
+++ netty/src/main/java/com/moon/integerdecoder/ToIntegerDecoder.java
@@ -0,0 +1,17 @@
+package com.moon.integerdecoder;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class ToIntegerDecoder extends ByteToMessageDecoder {
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out) throws Exception {
+        if (in.readableBytes() >= 4) {
+            out.add(in.readInt());
+        }
+    }
+}
---
+++ netty/src/main/java/com/moon/integerdecoder/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 自定义转整型编解码小例子
+ */
+package com.moon.integerdecoder;
